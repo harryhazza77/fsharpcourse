7ecea32e6f65c5c6865ac4a08d5686e2565c3344
harry <harry.patel@scribestar.com>
2014-06-19 14:21:39 +0100

    partial application

---
+++ ConsoleApplication1/Library1/PartialApplication.fsx
@@ -0,0 +1,34 @@
+﻿let add x y = x + y
+let multiply x y = x * y
+
+// partials
+let add2 = add 2
+let double = multiply 2
+
+[1..10]
+|> List.map add2
+|> List.map double
+
+// more common way which means you don't need to define lots of partials
+[1..10]
+|> List.map (add 2)
+|> List.map (multiply 2)
+
+// only loop the list once
+[1..10]
+|> List.map (add 2 >> multiply 2)
+
+
+let replace oldStr newStr (s:string) = 
+    s.Replace(oldValue=oldStr, newValue=newStr)
+
+let startsWith lookFor (s:string) = 
+    s.StartsWith(lookFor)
+
+let result = 
+    "hello"
+    |> replace "h" "j"
+    |> startsWith "j"
+
+["the";"quick";"brown";"fox"]
+    |> List.filter (startsWith "f")
