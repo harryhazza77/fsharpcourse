a346e0f80b02a8a57f875e64e3054540741fb45c
harry <harry.patel@scribestar.com>
2014-06-19 14:00:55 +0100

    decorator

---
+++ ConsoleApplication1/Library1/DecoratorPattern.fsx
@@ -0,0 +1,14 @@
+﻿let  add1 x = x + 1
+let p = printfn
+[1..10] |> List.map add1
+
+let logAdd1 x = 
+    p "x is %i" x 
+
+let genericLogger f x = 
+    p "x is %i" x
+    f x
+
+let logAdd1b = genericLogger add1
+
+10 |> logAdd1b
