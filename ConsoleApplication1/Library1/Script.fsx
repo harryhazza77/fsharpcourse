5f4c7fe543f7c48ea82098867fa73c1b44b1393b
harry <harry.patel@scribestar.com>
2014-06-19 13:31:29 +0100

    added another file for separation of examples

--- ConsoleApplication1/Library1/Script.fsx
+++ ConsoleApplication1/Library1/Script.fsx
@@ -21,8 +21,13 @@ type Person3 = {name:string; dob:DateTime}
 
 let p2 = {Person2.name="harry"; dob=DateTime.Now}
 let thisWillBeAPerson3BecauseItsTheLastOneDefined = {name="harry"; dob=DateTime.Now}
+let anotherp3 = {name="harry"; dob=DateTime.Now}
 
-// Define your library scripting code here
+// value equality for free!
+thisWillBeAPerson3BecauseItsTheLastOneDefined = anotherp3
+
+type Person4 = {name:string; dob:DateTime; email:string option}
+
 printfn "hello world %i" 42
 
 printfn "hello world %s" "42"
@@ -30,4 +35,47 @@ printfn "hello world %s" "42"
 let hellopartial = printfn "hello world %s"
 
 let add1 (x:int) = x + 1
+
+let tempInterfaceImpl = 
+    {new IDisposable with
+        member this.Dispose() = printfn "I'm disposed"}
+
+tempInterfaceImpl.Dispose()
+
+do 
+    printfn "started"
+    use tempInterfaceImpl = 
+        {new IDisposable with
+          member this.Dispose() = printfn "I'm disposed"}
+    printfn "ended"
+
+// this is 3 different implemenations of IPaymentMethod all with their own constructor!
+type PaymentMethods = 
+    | Cash
+    | Cheque of int
+    | Card of string * int   
+
+let paymethod1 = Cash
+
+// pattern matching (switch statement on drugs)
+let printPaymentMethod meth =
+    match meth with
+    | Cash -> printfn "type of Cash"
+    | Cheque cheqNo -> printf "type of cheque %i" cheqNo
+    | Card (name,num) -> printf "type of cheque %s %A" name num
+
+printPaymentMethod paymethod1
+
+let list1 = [2;3;4;5]
+let prepend = 1 :: list1
+let append = [4] @ list1
+
+let sumOfSquares =
+    let sq x = x * x
+    [1..100]
+    |> List.map sq
+    |> List.sum
+
+let aTuple = 1,2
+
 
---
+++ ConsoleApplication1/Library1/Script1.fsx
@@ -0,0 +1,22 @@
+﻿type Temp = 
+    | DegreesC of float
+    | DegreesF of float
+
+let toC input =
+    match input with 
+    | DegreesF f -> (f - 32.0) / 1.8 |> DegreesC
+    | _ -> input
+
+let isFever input =
+    match input with 
+    | DegreesC c -> c >= 38.0
+    | DegreesF f -> f >= 101.0
+
+DegreesC 100.0 |> isFever
+
+// equivalent ..
+isFever (DegreesC 100.0)
+DegreesF 103.0 |> isFever // best
+103.0 |> DegreesF |> isFever
+
+
