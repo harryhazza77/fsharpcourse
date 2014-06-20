72ee1edd32fd43debd91e177bd33078ad654bcb3
harry <harry.patel@scribestar.com>
2014-06-20 16:49:55 +0100

    agents!

--- day2/Day 2 - 2 Async Concurrent/2 TrafficLights/Agent.fsx
+++ day2/Day 2 - 2 Async Concurrent/2 TrafficLights/Agent.fsx
@@ -1,1 +1,36 @@
-﻿
+﻿type Agent<'T> = MailboxProcessor<'T>
+
+type ChatMessage = 
+    | Send of userName:string * message:string
+    | PrintHtml
+
+let chatAgent = Agent.Start(fun inbox ->
+    let rec loop msgs = async {
+        let! cmd = inbox.Receive()
+        match cmd with
+        | Send (u,m) ->
+            let newMsgs = (u,m)::msgs
+            return! loop newMsgs
+        | PrintHtml -> 
+            msgs 
+                |> Seq.iter (fun (u,m) -> printf "<li><strong>%s</strong>%s</li>" u m)
+            return! loop msgs
+    }
+    loop [] )
+
+chatAgent.Post(Send("Tomas", "Hello People"))
+chatAgent.Post(Send("Scott", "Hello back"))
+chatAgent.Post(PrintHtml)
+
+
+let statsAgent = Agent.Start(fun inbox -> 
+    let rec loop sum count = async {
+        let! value = inbox.Receive()
+        printfn "Avg = %f" (sum/count)
+        return! loop (sum + value) (count + 1.0) }
+        
+    loop 0.0 0.0 )
+
+let rnd = new System.Random()
+for i in 1.0 .. 100000.0 do
+    statsAgent.Post(rnd.NextDouble())
